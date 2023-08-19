37b12acda32fc3aec4364fa7d3cb9a3f92811f33
ylzz1997 <ylzz1997@outlook.com>
2023-08-20 00:16:36 +0800

    debug

--- models.py
+++ models.py
@@ -281,7 +281,7 @@ class SynthesizerTrn(nn.Module):
         # ssl prenet
         mel, c_lengths, c_max_length = self.preprocess(mel, c_lengths, mel.size(2))
         x_mask = torch.unsqueeze(commons.sequence_mask(c_lengths, c_max_length), 1).to(c.dtype)
-        x = (self.pre(c) + self.emb_uv(uv.long()).transpose(1,2) + vol + xg)[:,:,:c_lengths] * x_mask
+        x = (self.pre(c) + self.emb_uv(uv.long()).transpose(1,2) + vol + xg)[:,:,:c_max_length] * x_mask
         
         # f0 predict
         if self.use_automatic_f0_prediction:
